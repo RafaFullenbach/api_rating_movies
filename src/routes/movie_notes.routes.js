988f8e3abf9f7b51b7ebf85e40e2f1eddcc04331
Rafael Füllenbach <rafael.fullenbach12@gmail.com>
2023-09-22 18:02:12 -0300

    creating the delete movie notes method

--- src/routes/movie_notes.routes.js
+++ src/routes/movie_notes.routes.js
@@ -6,5 +6,6 @@ const movieNotesController = new MovieNotesController();
 
 movieNotesRoutes.get("/:id", movieNotesController.show);
 movieNotesRoutes.post("/:user_id", movieNotesController.create);
+movieNotesRoutes.delete("/:id", movieNotesController.delete);
 
 module.exports = movieNotesRoutes;
