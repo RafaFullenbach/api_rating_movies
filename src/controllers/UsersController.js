c0709f79b1ecabac21a738d5cb7e5bede09bdd90
Rafael Füllenbach <rafael.fullenbach12@gmail.com>
2023-09-22 15:16:11 -0300

    registering new user in the bank

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,12 +1,20 @@
+const sqliteConnection = require("../database/sqlite");
 const AppError = require("../utils/AppError");
 
 class UsersController {
-    create(request, response) {
+    async create(request, response) {
         const { name, email, password } = request.body;
+        const database = await sqliteConnection();
+        const checkUsersExists = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
 
-        if (!name) {
-            throw new AppError("Nome é obrigatório!");
+        if (checkUsersExists) {
+            throw new AppError("Este e-mail já está em uso.");
         }
+
+        await database.run(
+            "INSERT INTO users (name, email, password) VALUES (?,?,?)",
+            [name, email, password]
+        );
 
         return response.status(201).json();
     }
