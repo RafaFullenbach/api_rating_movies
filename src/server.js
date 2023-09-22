eccdc23054735c8fe3d412c6184d40063053cff4
Rafael Füllenbach <rafael.fullenbach12@gmail.com>
2023-09-22 12:57:06 -0300

    configuring application routes

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 const express = require("express");
-
+const routes = require("./routes");
 
 const app = express();
 
+app.use(routes);
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
