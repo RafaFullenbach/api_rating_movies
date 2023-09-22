1c7e7dbd36babdd22f28ec072e2a225351706227
Rafael Füllenbach <rafael.fullenbach12@gmail.com>
2023-09-22 15:32:15 -0300

    creating the update user method

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -20,6 +20,36 @@ class UsersController {
 
         return response.status(201).json();
     }
+
+    async update(request, response) {
+        const { name, email } = request.body;
+        const { id } = request.params;
+        const database = await sqliteConnection();
+        const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
+        const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+
+        if (!user) {
+            throw new AppError("Usuário não encontrado");
+        }
+
+        if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+            throw new AppError("Este e-mail já está em uso.");
+        }
+
+        user.name = name ?? user.name;
+        user.email = email ?? user.email;
+
+        await database.run(`
+            UPDATE users SET
+            name = ?,
+            email = ?,
+            updated_at = DATETIME('now')
+            WHERE id = ?`,
+            [user.name, user.email, id]
+        );
+
+        return response.json();
+    }
 }
 
 module.exports = UsersController;
