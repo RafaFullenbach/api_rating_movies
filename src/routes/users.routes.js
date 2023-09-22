1b09df6d67e2c83834f7f5fe49bf6d9e05cad46f
Rafael Füllenbach <rafael.fullenbach12@gmail.com>
2023-09-22 14:27:08 -0300

    creating Utils, creating AppError class

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -2,7 +2,6 @@ const { Router } = require("express");
 const UsersController = require("../controllers/UsersController");
 
 const usersRoutes = Router();
-
 const usersController = new UsersController();
 
 usersRoutes.post("/", usersController.create);
