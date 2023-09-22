3d6ddf89be653d2141f4c3fb979db84f39b83aea
Rafael Füllenbach <rafael.fullenbach12@gmail.com>
2023-09-22 15:42:28 -0300

    update user password

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -22,7 +22,7 @@ class UsersController {
     }
 
     async update(request, response) {
-        const { name, email } = request.body;
+        const { name, email, password, old_password } = request.body;
         const { id } = request.params;
         const database = await sqliteConnection();
         const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
@@ -36,6 +36,20 @@ class UsersController {
             throw new AppError("Este e-mail já está em uso.");
         }
 
+        if (password && !old_password) {
+            throw new AppError("Você precisa informar a senha antiga para definir a nova senha");
+        }
+
+        if (password && old_password) {
+            const checkOldPassword = await compare(old_password, user.password);
+
+            if (!checkOldPassword) {
+                throw new AppError("A senha antiga não confere.");
+            }
+
+            user.password = await hash(password, 8);
+        }
+
         user.name = name ?? user.name;
         user.email = email ?? user.email;
 
@@ -43,9 +57,10 @@ class UsersController {
             UPDATE users SET
             name = ?,
             email = ?,
+            password = ?,
             updated_at = DATETIME('now')
             WHERE id = ?`,
-            [user.name, user.email, id]
+            [user.name, user.email, user.password, id]
         );
 
         return response.json();
