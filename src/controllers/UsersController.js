1acf868af0dfb2a8515d14c9cab418327ca57ba1
Rafael Füllenbach <rafael.fullenbach12@gmail.com>
2023-09-22 15:19:18 -0300

    encrypting password with bcrypt library

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,11 +1,13 @@
 const sqliteConnection = require("../database/sqlite");
 const AppError = require("../utils/AppError");
+const { hash, compare } = require("bcrypt");
 
 class UsersController {
     async create(request, response) {
         const { name, email, password } = request.body;
         const database = await sqliteConnection();
         const checkUsersExists = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+        const hashedPassword = await hash(password, 8);
 
         if (checkUsersExists) {
             throw new AppError("Este e-mail já está em uso.");
@@ -13,7 +15,7 @@ class UsersController {
 
         await database.run(
             "INSERT INTO users (name, email, password) VALUES (?,?,?)",
-            [name, email, password]
+            [name, email, hashedPassword]
         );
 
         return response.status(201).json();
